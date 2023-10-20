244108367620c9a8cda04f871f01152934786131
Александр Расстегаев <pavelkrut17@gmail.com>
2023-10-20 12:19:18 +0500

    Лаб 6 готовая

--- ServicePage.xaml.cs
+++ ServicePage.xaml.cs
@@ -71,11 +71,6 @@ namespace RasstegaevAutoservice
             ServiceListView.ItemsSource = currentServices;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
-
         private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
             UpdateServices();
@@ -99,6 +94,25 @@ namespace RasstegaevAutoservice
         private void RButtonDown_Checked(object sender, RoutedEventArgs e)
         {
             UpdateServices();
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage(null));
+        }
+
+        private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Service));
+        }
+
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if(Visibility == Visibility.Visible)
+            {
+                RasstegaevServiceEntities1.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                ServiceListView.ItemsSource = RasstegaevServiceEntities1.GetContext().Service.ToList();
+            }
         }
     }
 }
